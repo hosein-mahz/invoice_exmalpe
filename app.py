c4c8d21dc577c83e2a06391bc0125a21132baa10
zarei <zarei@gmail.com>
2022-08-25 13:23:18 +0430

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+
+user_invose = {
+    "time" : 13,
+    "user" : 1,
+    "Price": 0,
+    "order": [
+        {
+            "id": 1,
+            "title":"kala1",
+            "number":2
+        },
+        {
+            "id": 2,
+            "title":"kala2",
+            "number":7
+        }
+    ]
+}
+
+warehouse =[
+    {
+        "id":1,
+        "titlte":"kala1",
+        "Price":1500,
+        "inventory":2
+    },
+    {
+        "id" : 2,
+        "title": "kala2",
+        "price":2100,
+        "inventory": 4
+    },
+    {
+        "id" : 3,
+        "title": "kala3",
+        "price":1000,
+        "inventory": 0
+    }
+]
